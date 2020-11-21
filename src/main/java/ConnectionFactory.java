7e7a9bce43cf02c1d03f14ba0fb093d6bdc6ed2a
ch <mkmsacc@outlook.com>
2020-11-21 10:09:03 +0700

    Add WebClient

---
+++ src/main/java/ConnectionFactory.java
@@ -0,0 +1,5 @@
+import java.io.InputStream;
+
+public interface ConnectionFactory {
+	InputStream getData() throws Exception;
+}
---
+++ src/main/java/WebClient.java
@@ -0,0 +1,20 @@
+import java.io.InputStream;
+
+public class WebClient {
+	public String getContent(ConnectionFactory connectionFactory) {
+		String workingContent;
+		StringBuffer content = new StringBuffer();
+		try (InputStream is = connectionFactory.getData()) {
+			int count;
+			while (-1 != (count = is.read())) {
+				content.append(new String(Character.toChars(count)));
+
+			}
+			workingContent = content.toString();
+		} catch (Exception e) {
+			return null;
+		}
+		return workingContent;
+
+	}
+}
